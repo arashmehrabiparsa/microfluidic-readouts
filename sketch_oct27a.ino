86a2e0422113f714ff67ca0d49587f675ea038c4
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-12-22 01:13:00 -0800

    Update sketch_oct27a.ino

--- sketch_oct27a.ino
+++ sketch_oct27a.ino
@@ -1,8 +1,4 @@
-#define _PWM_LOGLEVEL_       4
 
-#include "Teensy_PWM.h"
-
-#define USING_FLEX_TIMERS      true
 
 // Define PWM pins
 uint32_t PWM_Pins[] = { 4, 5, 6, 7, 0, 1, 2, 3, 8, 9, 10, 11, 28, 29, 36, 37, 13, 33 };
@@ -11,12 +7,12 @@ uint32_t PWM_Pins[] = { 4, 5, 6, 7, 0, 1, 2, 3, 8, 9, 10, 11, 28, 29, 36, 37, 13
 uint32_t ADC_Pins[] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17};
 
 // Same duty cycle and frequency for all pins
-float dutyCycle[]   = { 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f };  
+float dutyCycle[]   = { 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f,50.0f,50.0f };  
 float frequency = 2000.0f;  // Same frequency for all channels
 
 #define NUM_OF_PINS       ( sizeof(PWM_Pins) / sizeof(uint32_t) )
 
-Teensy_PWM* PWM_Instance[NUM_OF_PINS];
+// Teensy_PWM* PWM_Instance[NUM_OF_PINS];
 
 const int ledPin = LED_BUILTIN;  // Use built-in LED pin
 unsigned long previousMillis = 0;  
@@ -31,34 +27,20 @@ void setup() {
   for (int i = 0; i < 18; i++) {
     pinMode(ADC_Pins[i], INPUT);
   }
-  for (uint8_t index = 0; index < NUM_OF_PINS; index++) {
-    PWM_Instance[index] = new Teensy_PWM(PWM_Pins[index], frequency, dutyCycle[index]);
-    if (PWM_Instance[index]) {
-      PWM_Instance[index]->setPWM();
-    }
-  }
-
-  Serial.println("Pin\tPWM_freq\tDutyCycle\tActual Freq");
-  for (uint8_t index = 0; index < NUM_OF_PINS; index++) {
-    if (PWM_Instance[index]) {
-      Serial.print(PWM_Pins[index]);
-      Serial.print("\t");
-      Serial.print(frequency);
-      Serial.print("\t\t");
-      Serial.print(dutyCycle[index]);
-      Serial.print("\t\t");
-      Serial.println(PWM_Instance[index]->getActualFreq(), 4);
-    }
+  for (uint8_t i = 0; i < NUM_OF_PINS; i++) {
+  // Set the frequency for each pin
+  analogWriteFrequency(PWM_Pins[i], frequency);
+  // Set the duty cycle (value between 0 and 255)
+  analogWrite(PWM_Pins[i], (uint8_t)(dutyCycle[i] / 100.0f * 255));
   }
 }
 
+
+
+
 void loop() {
   // Blink the LED
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    digitalWrite(ledPin, !digitalRead(ledPin));  // Toggle LED state
-  }
+
 
   // Capture the PWM signals using ADC on defined ADC pins
   Serial.println("Capturing PWM signals from ADC pins:");
